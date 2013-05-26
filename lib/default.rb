104018d0bbc88d91e8e6120d68c0b931144ed251
Ruben Verborgh <ruben@verborgh.org>
2013-05-26 09:22:19 +0200

    Add papers.

--- lib/default.rb
+++ lib/default.rb
@@ -6,7 +6,8 @@ def show_paper id
   paper = @items["/papers/#{id}/"]
   authors = paper[:author]
   '<dd itemscope itemtype="http://schema.org/ScholarlyArticle">' +
-  '<span itemprop="name">' + h(paper[:title]) + '</span>' +
+  "<a href='/papers/salad2013-#{h id[/\d+$/]}.pdf'>" +
+  "<span itemprop='name'>#{paper[:title]}</span></a>" +
       authors.to_a.map{ |a| h "#{a.first} #{a.prefix} #{a.last} #{a.suffix}".strip }.
         map{ |a| "<span itemprop='author'>#{a}</span>" }.
         join(authors.length <= 2 ? ' and ' : ', ').
